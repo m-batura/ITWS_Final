5dfef2a8e6ca302460ebc1abc19095e0b3e352f0
m-batura <mikhail.batura@gmail.com>
2025-06-07 09:22:37 +0200

    Initial commit

---
+++ src/main/java/com/example/ITWS_Final/ItwsFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.ITWS_Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItwsFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItwsFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ITWS_Final/ItwsFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ITWS_Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItwsFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
